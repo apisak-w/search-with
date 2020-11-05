eb8ca5acadf28f6d905a88c13a6b5c633046adf2
apisak-w <apisak.wongkhempetch@gmail.com>
2020-11-05 17:08:43 +0700

    Added search engine list

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,13 @@
 import { useRouter } from 'next/router'
 import { Fragment } from 'react';
 import Header from '../components/Header';
+import data from '../data/searchEngine.json';
+
+interface ISearchEngine {
+    name: string,
+    icon: string,
+    url: string
+}
 
 export default function Search({ searchQuery }) {
     const router = useRouter();
@@ -17,6 +24,18 @@ export default function Search({ searchQuery }) {
                 <div className="row">
                     <div className="portion whole">
                         <p>Search: {query}</p>
+                    </div>
+                    <div className="row">
+                        {data.map((se: ISearchEngine, i: number) => {
+                            return (
+                                <div className="portion">
+                                    <a href={se.url + query} className="ff-button bg-blue text-white" key={i} target="_blank">
+                                        <img src={"./assets/icons/" + se.icon} alt={se.name} style={{width: "15px", height: "15px"}}/>
+                                        &nbsp;{se.name}
+                                    </a>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </section>
